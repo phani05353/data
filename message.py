0e93eed053f4f4c6df3d7de98f576a1905164e0a
phani05353 <phani05353@gmail.com>
2020-10-18 02:23:25 -0400

    Add files via upload

--- message.py
+++ message.py
@@ -5,7 +5,7 @@ import psutil
 import psycopg2
 from datetime import datetime
 
-Server = 'PI-2'
+Server = 'PI-3'
   
 
 temp_c = os.popen("vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*'").readline()
@@ -20,27 +20,23 @@ memory_avail = psutil.virtual_memory().available * 100 / psutil.virtual_memory()
 
   # Print and stream
   
-conn = psycopg2.connect(database="performance", user='pi', password='Sox2017!', host='192.168.0.235', port= '5432')
+conn = psycopg2.connect(database="performance", user='pi', password='Sox2017', host='127.0.0.1', port= '5432')
 cur = conn.cursor()
 today = datetime.now()
-query = """INSERT INTO server_stats VALUES (%s, %s, %s)"""
-values = (Server,temp_f, today)
+query = """INSERT INTO server_status VALUES (%s, %s, %s,%s)"""
+values = (Server,today,temp_c,"server_temperature")
 cur.execute(query, values)
 conn.commit()
-query = """INSERT INTO server_stats_cpu VALUES (%s, %s, %s)"""
-values = (Server,cpu, today)
+query = """INSERT INTO server_status VALUES (%s, %s, %s,%s)"""
+values = (Server,today,cpu,"server_cpu")
 cur.execute(query, values)
 conn.commit()
-query = """INSERT INTO server_stats_memory_used VALUES (%s, %s, %s)"""
-values = (Server,memory_used, today)
-cur.execute(query, values)
-conn.commit()
-query = """INSERT INTO server_stats_memory_avail VALUES (%s, %s, %s)"""
-values = (Server,memory_avail, today)
+query = """INSERT INTO server_status VALUES (%s, %s, %s,%s)"""
+values = (Server,today,memory_used, "server_memory_used")
 cur.execute(query, values)
 conn.commit()
 query = """INSERT INTO job_status VALUES (%s, %s, %s)"""
-values = ('pi2_stats','true', today)
+values = ('pi3_stats','true', today) 
 cur.execute(query, values)
 conn.commit()
 conn.close()
